91c168207e9552cb599ead09fd47716e0abc615a
Applehihi <48282673+Applehihi@users.noreply.github.com>
2022-07-26 23:31:06 +0800

    More functionality for linked list

--- data_structures/linked_list/cpp/Linked_List.hpp
+++ data_structures/linked_list/cpp/Linked_List.hpp
@@ -9,11 +9,15 @@
 template <class T>
 class Linked_List {
 public:
-    void push(T p_data);
     void append(T p_data);
-    void begin(void);
-    void traverse(void);
+    void push(T p_data);
+    void insert(T p_data);
+    void remove(void);
+    void clear(void);
     T getData(void);
+    void next(void);
+    Node<T>* begin(void);
+    bool isEmpty(void);
 private:
     Node<T>* m_head = nullptr;
     Node<T>* m_current = nullptr;
@@ -24,21 +28,14 @@ private:
 //**********
 
 template <class T>
-void Linked_List<T>::push(T p_data) {
-    Node<T>* new_node = new Node<T>(p_data);
-    new_node->setNext(this->m_head);
-    this->m_head = new_node;
-}
-
-template <class T>
 void Linked_List<T>::append(T p_data) {
     //If list is initially empty
-    if(this->m_head == nullptr) {
-        this->m_head = new Node<T>(p_data);
+    if(this->isEmpty()) {
+        m_head = new Node<T>(p_data);
         return;
     }
     //Traverse to end of list
-    Node<T>* current = this->m_head;
+    Node<T>* current = m_head;
     while(current->getNext() != nullptr) {
         current = current->getNext();
     }
@@ -47,18 +44,82 @@ void Linked_List<T>::append(T p_data) {
 }
 
 template <class T>
-void Linked_List<T>::begin(void) {
-    this->m_current = this->m_head;
+void Linked_List<T>::push(T p_data) {
+    Node<T>* new_node = new Node<T>(p_data);
+    new_node->setNext(m_head);
+    m_head = new_node;
 }
 
 template <class T>
-void Linked_List<T>::traverse(void) {
-    this->m_current = m_current->getNext();
+void Linked_List<T>::insert(T p_data) {
+    //Handle case where list is initially empty
+    if(this->isEmpty()) {
+        this->push(p_data);
+        return; //Short circuit
+    }
+    //Invalid operation, effectively noop
+    if(m_current == nullptr) {
+        return; //Short circuit
+    }
+    Node<T>* new_node = new Node<T>(p_data);
+    new_node->setNext(m_current->getNext());
+    m_current->setNext(new_node);
+}
+
+template <class T>
+void Linked_List<T>::remove(void) {
+    //Handle case where list is empty
+    if(this->isEmpty()) {
+        return; //Noop
+    }
+    //Handle case where current active node is the start of list
+    if(m_head == m_current) {
+        m_head = m_current->getNext();
+        delete m_current;
+        return; //Short circuit
+    }
+    Node<T>* current = m_head;
+    Node<T>* previous = nullptr;
+    while(current != m_current) {
+        previous = current;
+        current = current->getNext();
+    }
+    previous->setNext(current->getNext());
+    delete current;
+    m_current = previous->getNext();
+}
+
+template <class T>
+void Linked_List<T>::clear(void) {
+    Node<T>* current = m_head;
+    Node<T>* to_delete = nullptr;
+    while(current != nullptr) {
+        to_delete = current;
+        current = current->getNext();
+        delete to_delete;
+    }
+    m_head = nullptr;
 }
 
 template <class T>
 T Linked_List<T>::getData(void) {
-    return this->m_current->getData();
+    return m_current->getData();
+}
+
+template <class T>
+void Linked_List<T>::next(void) {
+    m_current = m_current->getNext();
+}
+
+template <class T>
+Node<T>* Linked_List<T>::begin(void) {
+    m_current = m_head;
+    return m_current;
+}
+
+template <class T>
+bool Linked_List<T>::isEmpty(void) {
+    return m_head == nullptr;
 }
 
 
